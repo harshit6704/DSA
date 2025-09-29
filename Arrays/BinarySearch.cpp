28d09d0b79f7d30ef33d049046d3f8bd08524896
Harshit Yadav <harshityadav6704@gmail.com>
2025-09-29 15:55:56 +0530

    Binary-Search

---
+++ Arrays/BinarySearch.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(vector<int>&arr,int n,int k){
+int start=0;
+int end=n-1;
+int mid=start+(end-start/2);
+for(;start<=end;){
+    if(arr[mid]==k){
+        return mid;
+    }
+    else if(arr[mid]<k){
+        start=mid+1;
+    }
+    else{
+        end=mid-1;
+    }
+    mid=start+(end-start/2);
+}
+return -1;
+}
+int main(){
+int n;cin>>n;
+vector<int>arr(n);
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int k;cin>>k;
+cout<<"Searched element is at index "<<binarySearch(arr,n,k);
+}
