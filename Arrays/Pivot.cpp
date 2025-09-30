aed61f49676d47199c5c89142cc93584fd539a31
Harshit Yadav <harshityadav6704@gmail.com>
2025-09-30 21:41:53 +0530

    Pivot Element

---
+++ Arrays/Pivot.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int pivot(vector<int>& arr,int n){
+int start=0;
+int end=n-1;
+int mid=start+(end-start)/2;
+for(;start<end;){
+    if(arr[mid]>=arr[0]){
+        start=mid+1;
+    }
+    else{
+        end=mid;
+    }
+    mid=start+(end-start)/2;
+}
+return start;
+}
+int main(){
+int n;cin>>n;
+vector<int>arr(n);
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}  
+cout<<pivot(arr,n);
+}
